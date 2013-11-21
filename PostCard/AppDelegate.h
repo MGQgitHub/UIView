b2e9b11e5c2ae93387d7b5752185ab5cf58f504c
MGQgitHub <15242308408@163.com>
2013-11-21 21:29:22 +0800

    肤色色入

---
+++ PostCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PostCard
+//
+//  Created by qianfeng on 13-11-19.
+//  Copyright (c) 2013年 miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostCard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PostCard
+//
+//  Created by qianfeng on 13-11-19.
+//  Copyright (c) 2013年 miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
